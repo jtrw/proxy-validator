3fba7b0aa7e1ad3906d02e439c380b917795d958
Nil Borodulia <nil.b@oxtech.org>
2023-03-24 13:49:18 +0300

    add tests

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -2,12 +2,36 @@
 
 namespace Jtrw\ProxyValidator\Tests;
 
+use GuzzleHttp\Client;
+use Jtrw\ProxyValidator\Validator;
 use PHPUnit\Framework\TestCase;
 
 class ValidatorTest extends TestCase
 {
-    public function testFirst()
+    public function testFailedProxy()
     {
-        self::assertTrue(true);
+        $validator = new Validator(new Client());
+        $proxyStr = "127.0.0.1:27701:user:pass:http";
+        $response = $validator->validate($proxyStr);
+        
+        static::assertFalse($response->isValid());
+    
+        $errorsDto = $response->getErrors();
+        
+        static::assertEquals($proxyStr, $errorsDto->getProxyName());
+        static::assertNotEmpty($errorsDto->getMessage());
+    }
+    
+    public function testNotValidProxyStr()
+    {
+        $validator = new Validator(new Client());
+        $proxyStr = "127.0.0.1";
+        $response = $validator->validate($proxyStr);
+    
+        static::assertFalse($response->isValid());
+    
+        $errorsDto = $response->getErrors();
+        
+        static::assertEquals($errorsDto->getMessage(), "Port Not Found");
     }
 }
